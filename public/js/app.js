84336c0e688ac63cd8d9334b917c8d175f79340f
DaJuan Harris <dajuancancode@gmail.com>
2019-06-20 09:50:13 -0400

    Initialize project with git for deployment

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const messageOne   = document.querySelector('#messageOne')
+const messageTwo     = document.querySelector('#messageTwo')
+const weatherForm = document.querySelector('#searchForm')
+const search      = document.querySelector('input')
+
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const location = search.value
+
+  messageOne.textConent = 'Loaing...'
+  messageTwo.textContent = ''
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+  .then(res => {res.json().then(data => {
+    if(data.error){
+      messageOne.textContent = data.error
+    }else {
+      messageOne.textContent = data.forecast
+      messageTwo.textContent = data.location
+    }
+  })})
+})
